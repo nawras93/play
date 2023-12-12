01e0d22a9d0eadcaa4ec3dfdaa64e9e4ab0416ce
nawras93 <ismailmoh221@gmail.com>
2023-12-12 07:51:37 +0300

    AutoAnimate first commit

---
+++ src/components/Example1.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue"
+import autoAnimate from "@formkit/auto-animate"
+
+let numbers = ref(Array.from({ length: 100 }, (y, i) => i))
+function shuffle() {
+    numbers.value.sort(() => 0.5 - Math.random())
+}
+let container = ref();
+onMounted(() => {
+    autoAnimate(container.value);
+})
+</script>
+
+<template >
+    <section ref="container" class="flex flex-wrap gap-4">
+        <div v-for="number in numbers" :key="number" v-text="number" class="bg-gray-200 w-12 h-12 flex items-center justify-center">
+        </div>
+    </section>
+    <div class="fixed bottom-4 right-4">
+        <!-- <button class="bg-black rounded text-white" @click="numbers.unshift(100)">
+            Add
+        </button>
+        <button class="bg-black rounded text-white" @click="numbers.push(100)">
+            Add
+        </button> -->
+        <button class="bg-black rounded text-white" @click="shuffle">
+            Shuffle
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Example2.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+let todos = ref([
+    {id: 1, body: 'Go to the store', completed: false},
+    {id: 2, body: 'Learn about AutoAnimate', completed: false},
+    {id: 3, body: 'Pick up kids from school', completed: false},
+    {id: 4, body: 'Finish homework assignment', completed: false},
+])
+
+let newTodo = ref('');
+
+function addTodo() {
+    todos.value.push({
+        id: todos.length + 1,
+        body: newTodo.value,
+        completed: false,
+    });
+    newTodo.value = '';
+}
+
+function shuffleTodos() {
+    todos.value.sort(() => 0.5 - Math.random())
+}
+</script>
+
+<template >
+    <section>
+        <h1>Todos</h1>
+
+        <div class="mt-4" v-auto-animate>
+            <div v-for="todo in todos" :key="todo.id" class="flex justify-between gap-x-6 bg-gray-200 mb-4 px-3 py-2 rounded">
+                <p>{{ todo.body }}</p>
+                
+                <input type="checkbox" :checked="todo.completed">
+            </div>
+
+            <div>
+                <form @submit.prevent="addTodo">
+                    <textarea name="" id="" cols="30" rows="4" class="border" v-model="newTodo" required></textarea>
+                    <div>
+                        <button>Submit</button>
+                        <button @click="shuffleTodos">Shuffle</button>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
